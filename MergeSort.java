aa91291ff88f0ff2abeaeab96903435118199ab4
JUAN VICTOR GOMES ACOSTA <j.acosta@edu.pucrs.br>
2022-03-16 20:18:08 -0300

    ex1

--- MergeSort.java
+++ MergeSort.java
@@ -1,45 +1,53 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class MergeSort {
 
     public static void main(String[] args) {
         ArrayList<Integer> numbers = new ArrayList<>();
-        for (int i = 0; i < 32; i++) {
-            numbers.add((int)Math.random());
+        Random rand = new Random();
+        for (int i = 0; i < 10; i++) {
+            //numbers.add(Math.random());
+            numbers.add(rand.nextInt(100));
         }
+        System.out.println("lista original: "+ numbers);
+        int count = 0;
         long start = System.currentTimeMillis();
-        mergeSort(numbers);
+        System.out.println("\n"+mergeSort(numbers, count));
         long time = System.currentTimeMillis() - start;
-        System.out.println("Tempo gasto: " + time);
-
+        System.out.println("Tempo gasto: " + time);      
     }
 
-   public static List<Integer> mergeSort(List<Integer> sortedList) {
-       if (sortedList.size()==1) {
-           return sortedList;
-       }
+    public static List<Integer> mergeSort(List<Integer> sortedList, int count) {
+        count++;    
+        if (sortedList.size()==1) {
+            System.out.println(count);
+            return sortedList;       
+        }
+       
        int halfSize = sortedList.size()/2; 
        List<Integer> a = sortedList.subList(0,halfSize);
        List<Integer> b = sortedList.subList(halfSize,sortedList.size());
-       a = mergeSort(a);
-       b = mergeSort(b);
-       sortedList = merge(a,b); 
+       a = mergeSort(a,count);
+       b = mergeSort(b,count);
+       sortedList = merge(a,b);
        return sortedList;  
     }
 
     public static List<Integer> merge(List<Integer> a, List<Integer> b){
         ArrayList<Integer> merged = new ArrayList<>();
-        int i = 0;
-        int j = 0;
+        int i = 0, j = 0;
         for (int counter = 0; counter <(a.size()*2); counter++) {
             if (j>=b.size()) {
-                merged.add(a);
+                merged.add(a.get(i));
+                i++;
             }
-            else if (condition) {
-                
+            else if (i>=a.size()) {
+                merged.add(b.get(j));
+                j++;
             }
-            if (a.get(i)<= b.get(j)) {
+            else if (a.get(i)<= b.get(j)) {
                 merged.add(a.get(i));
                 i++;
             }
@@ -51,4 +59,12 @@ public class MergeSort {
         return merged;
     }
     
+    /*public static int maiorValor(List<Integer>a, int n){
+        int max = a.get(0);
+        for (int i = 1; i < n; i++) {
+           if( a.get(i) > max ) 
+            max = a.get(i);
+        }
+        return max;
+    }*/
 }
